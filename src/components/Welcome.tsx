2f8fb561548991899b99c70b193252ce510cf3cf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 22:37:21 -0300

    feat: Adicionado título, frase emocional e emoji, estilização

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,17 +1,37 @@
 export function Welcome(){
     return(
-        <>
-        <h1>Bem vindo  na Trackfitmini</h1>
+        
 
-     
+        <div style={{
+            border:'4px solid black',
+            textAlign: 'center',
+            backgroundColor: 'gray',
+            padding: '20px',
+            margin: '20px',
+            textShadow: '1px 1px 0 black',
+            fontFamily:'cursive',
+            fontSize: '20px',
+            display: "grid",
+            columnGap: "30px",
+            boxShadow: '5px 5px 10px 0px rgba(0, 0, 0, 0.5)',
+            gap: '10px'}}>
 
-       
+
+        
+            <h1>Bem vindo na TrackFit Mini</h1>
+        
+
+        
+            <h2>A mudança depende de você mesmo!!não desista, continue se esforçando e veja como seus resultados começam a se transformar.Tudo depende de você mesmo, a disciplina em fazer o que tem que ser feito todos os dias sem preguiça e sim levar aquilo como uma missão de vida </h2>
+            <h2>🏋️‍♂️🏃‍♂️🚴</h2>
+
+
+        </div>
 
 
 
 
-
-        </>
+        
     )
 
 
